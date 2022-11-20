35601202b59f0fd718743bd1b658699f11c089a9
Robert <rpalfini@uci.edu>
2022-11-20 12:50:13 -0800

    updated TSP generator with 11/17/22 version from class

--- Generator/generate_travelling_salesman_problem.py
+++ Generator/generate_travelling_salesman_problem.py
@@ -3,13 +3,18 @@ import numpy as np
 
 def write_distance_matrix(n, mean, sigma):
     distance_matrix = np.zeros((n, n))
+    random_distance = []
+    num_distance = int(n * (n-1) / 2)
+    for _ in range(num_distance):
+        distance = 0
+        while distance <= 0:
+            distance = np.random.normal(mean, sigma)
 
-    for row in range(n):
-        for col in range(n):
-            distance = 0
-            while distance <= 0:
-                distance = np.random.normal(mean, sigma)
-                distance_matrix[row][col] = distance
+        random_distance.append(distance)
+    
+    iu = np.triu_indices(n, 1)
+    distance_matrix[iu] = random_distance
+    distance_matrix += distance_matrix.T
 
     np.savetxt(
         f"{n}_{mean}_{sigma}.out",
